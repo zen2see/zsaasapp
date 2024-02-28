4b21d911d835335532eebb21382a412a59a4105a
zen2see <zen2se@gmail.com>
2024-02-28 14:02:58 -0500

    INITIAL COMMIT - Working login and base home page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink, LogoutLink} from '@kinde-oss/kinde-auth-nextjs/components'
+import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
+
+export async function Navbar() {
+    const { isAuthenticated } = getKindeServerSession() 
+    return (
+        <nav className='border-b bg-background h-[10vh] flex items-center'>
+            <div className='container flex items-center justify-between'>
+                <Link href='/'>
+                    <h1 className='font-bold text-3xl'>zSaaSapp</h1>
+                </Link>
+                <div className='flex items-center gap-x-5'>
+                    <ThemeToggle />
+                    {(await isAuthenticated()) ? (
+                        <LogoutLink>
+                            <Button>Log Out</Button>
+                        </LogoutLink>
+                    ) : (
+                        <div className='flex items-center gap-x-5'>
+                            <LoginLink>
+                                <Button>Sign In</Button>
+                            </LoginLink>
+                            <RegisterLink>
+                                <Button variant='secondary'>Sign Up</Button>
+                            </RegisterLink> 
+                        </div>
+                    )}
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components'
+
+export default function DashboardPage() {
+    return (
+        <section className='flex items-center justify-center bg-bakcgorund h-[90vh]'>
+            <div className='relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12'>
+                <div className='max-w-3xl mx-auto text-center'>
+                    <div>
+                        <span className='w-auto px-6 py-3 rounded-full bg-gray-200'>
+                            <span className='text-sm font-medium text-black'>Sort your notes easily</span>
+                        </span>
+                        <h1 className='mt-8 text-5xl font-extrabold tracking-tight lg-text-6xl'>Create Notes with ease</h1>
+                        <p className='max-w-l mx-auto mt-8 text-base lg:text-xl text-secondary-foreground'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,  
+                            modi dolorem dolorum deserunt nesciunt doloremque placeat
+                        </p>
+                    </div>
+                    <div className='flex justify-center max-w-sm mx-auto mt-10'>
+                      <RegisterLink>
+                        <Button size='lg' className='w-full'>
+                            Sign Up for free
+                        </Button>
+                      </RegisterLink>
+                    </div> 
+                </div>
+            </div>
+        </section>
+    )
+}  
