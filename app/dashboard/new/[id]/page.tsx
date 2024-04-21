38e455278795c0611947e40fdb78f331fb2a20ae
zen2see <zen2se@gmail.com>
2024-04-20 22:39:19 -0400

    FIX - CORRECTED LABEL IMPORT ON DASHBOARD

--- app/dashboard/new/[id]/page.tsx
+++ app/dashboard/new/[id]/page.tsx
@@ -1,48 +1,64 @@
-import { SubmitButtons } from '@/app/components/SubmitButtons'
-import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
-import Link from 'next/link'
-import prisma from "@/app/lib/db"
-import  {getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { redirect } from 'next/navigation'
+import { SubmitButtons } from "@/app/components/SubmitButtons";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
-export default async function NewNoteRoute() {
+import Link from "next/link";
+import  {getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import prisma from '@/app/lib/db'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from "next/cache";
+import { Label } from "@/components/ui/label";
+
+async function getData({ userId, noteId }: { userId: string; noteId: string }) {
+  const data = await prisma.note.findUnique({
+    where: {
+      id: noteId,
+      userId: userId,
+    },
+    select: {
+      title: true,
+      description: true,
+      id: true,
+    },
+  })
+  return data
+}
+
+export default async function DynamicRoute({
+  params,
+}: { 
+  params: { id: string }
+}) {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
+  const data = await getData({userId: user?.id as string, noteId: params.id })
   async function postData(formData: FormData) {
     'use server'
-    if(!user) {
-      throw new Error('Not authorized')
-    }
+    if (!user) throw new Error("You are not authorized")
     const title = formData.get('title') as string
     const description = formData.get('description') as string
-    await prisma.note.create({
+    await prisma.note.update({
+      where: {
+        id: data?.id,
+        userId: user.id
+      },
       data: {
-        userId: user?.id,
         description: description,
         title: title,
       },
     })
+    revalidatePath('/dashboard')
     return redirect('/dashboard')
-  } 
-
+  }
   return (
     <Card>
       <form action={postData}>
-        <CardHeader>
-          <CardTitle>New Note</CardTitle>
+        <CardHeader> 
+          <CardTitle>Edit Note</CardTitle>
           <CardDescription>
-             You can create your new notes here.
+             You can edit your notes here.
           </CardDescription>
         </CardHeader>
         <CardContent className='flex flex-col gap-y-5'>
@@ -53,6 +69,7 @@ export default async function NewNoteRoute() {
               type='text'
               name='title'
               placeholder='Title'
+              defaultValue={data?.title}
             />
           </div>
           <div className='flex flex-col gap-y-2'>
@@ -61,6 +78,7 @@ export default async function NewNoteRoute() {
               name='description' 
               placeholder='Describe your note as you want'
               required
+              defaultValue={data?.description}
             />
           </div>
         </CardContent>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,51 +1,85 @@
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 import prisma from '@/app/lib/db'
-import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
-import { File } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { Edit, File, Trash } from 'lucide-react'
+import { Card } from "@/components/ui/card"
 
 async function getData(userId: string) {
-   const data = await prisma.note.findMany({
-      where: {
-         userId: userId
-      },
-      orderBy: {
-         createdAt: 'desc',
-      }
-   })
-   return data
+  const data = await prisma.note.findMany({
+    where: {
+      userId: userId
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+  return data
 }
 export default async function DashboardPage() {
-   const { getUser } = getKindeServerSession()
-   const user = await getUser()
-   const data = await getData(user?.id as string)
-   return (
-      <div className='grid items-start gap-y-8'>
-         <div className='flex items-center justify-between px-2'>
-            <div className='grid gap-1'>
-               <h1 className='text-3xl md:text-4xl'>Your Notes</h1>
-               <p className='text-lg text-muted-foreground'>
-                  Here you can see and create new notes.
-               </p>
-            </div>
-            <Button asChild>
-               <Link href='/dashboard/new'>Create a new Note</Link>
-            </Button>
-         </div>
-         {data.length < 1 ? (
-            <div className='flex min-h-[400px] flex-col items-center justify-center
-              rounded-md border border-dashed p-8 text-center animate-in fade-in-50'>
-              <div className='flex h-20 w-20 items-center justify-center rounded-full bg-primary/10'>
-                 <File className='w-10 h-10 text-primary' />
-              </div>
-              <h2 className='mt-6 text-xl font-smibold'>You don't have any notes yet.</h2>
-              <p className='mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto'>
-                 You currently don't have any notes. Notes you create will be shown here.
-              </p>
-            </div>
-         ) : (
-            <div></div>
-         )}
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  const data = await getData(user?.id as string)
+  return (
+    <div className='grid items-start gap-y-8'>
+      <div className='flex items-center justify-between px-2'>
+        <div className='grid gap-1'>
+          <h1 className='text-3xl md:text-4xl'>Your Notes</h1>
+          <p className='text-lg text-muted-foreground'>
+            Here you can see and create new notes.
+          </p>
+        </div>
+        <Button asChild>
+          <Link href='/dashboard/new'>Create a new Note</Link>
+        </Button>
       </div>
-   )
+      {data.length < 1 ? (
+        <div className='flex min-h-[400px] flex-col items-center justify-center
+              rounded-md border border-dashed p-8 text-center animate-in fade-in-50'>
+          <div className='flex h-20 w-20 items-center justify-center rounded-full bg-primary/10'>
+            <File className='w-10 h-10 text-primary' />
+          </div>
+          <h2 className='mt-6 text-xl font-smibold'>You don't have any notes yet.</h2>
+          <p className='mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto'>
+            You currently don't have any notes. Notes you create will be shown here.
+          </p>
+          <Button asChild>
+            <Link href='/dashboard/new'>Create a new Note</Link>
+          </Button>
+        </div>
+      ) : (
+        <div className='flex flex-col gap-y-4'>
+          {data.map((item) => (
+            <Card
+              className='flex items-center justify-between p-4'
+              key={item.id}
+            >
+              <div>
+                <h2 className='font-semibold text-xl text-primary'>
+                  {item.title}
+                </h2>
+                <p>
+                  {new Intl.DateTimeFormat('en-US', {
+                    dateStyle: 'full',
+                  }).format(new Date(item.createdAt))}
+                </p>
+              </div>
+              <div className='flex gap-x-4'>
+                <Link href={`/dashboard/new/${item.id}`}>
+                  <Button variant='outline' size='icon'>
+                    <Edit className='w-4 h-4' />
+                  </Button>
+                </Link>
+                <form>
+                  <Button variant={'destructive'} size='icon'>
+                    <Trash className='h-4 w-4' />
+                  </Button>
+                </form>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }  
